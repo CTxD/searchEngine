333356e62ec2180b580cfb3316cbe6751f2b603f
CTxD <casper.thomsen100@gmail.com>
2019-01-03 21:23:02 +0100

    Read urls from a file

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+import json
+
 import crawler as Crawler
 import bfs as Bfs
 
+
 # Set of urls, that we want to crawl
 url_set = set()
-url_set.add("http://www.google.dk/")
-url_set.add("http://www.facebook.com/")
-url_set.add("https://stackoverflow.com/")
+
+# Import the urls from crawlLinks.json file
+try:
+    with open("crawlLinks.json", 'r') as inFile:
+        data = json.load(inFile)
+    
+    for url in data["urls"]:
+        url_set.add(url)
+except:
+    EnvironmentError("An error occured!")
+
+
 
 # Init the crawler with the set of urls
 crawler = Crawler.Crawler(url_set)
-crawler_result = crawler.crawl(2, False)
+crawler_result = crawler.crawl(3, False)
 
 
 next = crawler_result.next()
@@ -17,6 +29,3 @@ while next != None:
     print(next.data["url"])
 
     next = crawler_result.next()
-
-for node in crawler_result.root.children:
-    print(node.data["content"])
